b33e1a8f1c88c1490d27d0a8f31ef158c7719f73
Atze de Vries <atze.devries@naturalis.nl>
2016-11-26 14:14:05 +0100

    up

--- block-storage-cinder.py
+++ block-storage-cinder.py
@@ -2,8 +2,8 @@
 
 # Script to get stats about block storage managed by OpenStack Cinder
 
-from lib import keystone as KeyStone
-from lib import nova 
+from lib.keystone import KeyStone
+from lib import nova
 from lib import cinder
 from lib import log
 from lib import config
@@ -14,7 +14,7 @@ auth_url = 'https://' + config.get('admin_endpoint_ip') + ':5000/v3'
 auth_url_v2 = 'https://' + config.get('admin_endpoint_ip') + ':5000/v2.0'
 ks_username = config.get('os_username')
 ks_password = config.get('os_password')
-project_name = config.get('project_name')
+project_name = config.get('os_project_name')
 ca_bundle = config.get('ca_bundle_file')
 
 ### End Settings
